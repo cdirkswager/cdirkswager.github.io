41911b0062e06f894b33e0ca24d1b08ac2fc7f0c
Chris Dirkswager <59585518+cdirkswager@users.noreply.github.com>
2026-06-01 18:38:56 -0500

    Updates 1

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Hunt-Website">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
